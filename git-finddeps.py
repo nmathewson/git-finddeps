99d7025c98cd0ce742487007fdb9de05fa627d60
Nick Mathewson <nickm@torproject.org>
2012-09-04 18:14:31 -0400

    Add the rest of the license and a disclaimer

--- git-finddeps.py
+++ git-finddeps.py
@@ -12,12 +12,38 @@ modification, are permitted provided that the following conditions are met:
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation
+are those of the authors and should not be interpreted as representing
+official policies, either expressed or implied, of the author or
+anyone else.
 """
 
 import random
 import subprocess
 import sys
 import os
+
+#
+#
+#  ALL COMMENTS ARE PROBABLY BOGUS.
+#
+#
+#
+
+
 
 # Problem: Say we have some branch B and a root branch R.  So the
 # "depends on" relationship on the commits in R..B is a partial ordering,
@@ -40,7 +66,6 @@ import os
 #    the subsequence of the original sequence containing only commits
 #    from that set, applies in order and passes some given test.
 #    Evaluate this lazily.
-
 
 
 class DependencyLearner:
